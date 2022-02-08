dcbfc899377734d230ed07984dfb35f9d46a423c
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-08 16:47:33 -0500

    Implemented the server and launched it

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require("./api/server");
+
+const port = 80;
+
+server.listen(port, () => {
+  console.log("Server listening on port 80 😍");
+});
