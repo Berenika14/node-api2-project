fbf285711a81b3b7bd5f2af66d96fff0501c1df7
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-02-08 21:55:13 -0500

    Finished the project

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -106,4 +106,24 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      const message = await Post.findCommentById(req.params.id);
+      res.json(message);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post could not be removed",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
+
 module.exports = router;
